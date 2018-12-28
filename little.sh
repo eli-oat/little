fe81e2528b935dd817c5e787c62aed5f14da3b84
Eli Mellen <eli@beardandbeanie.com>
2018-12-27 20:43:59 -0500

    added a basic cache of references to posts to use when building archive pages, etc

--- little.sh
+++ little.sh
@@ -10,19 +10,19 @@ USER_INPUT="$@"
 if [ -n "$USER_INPUT" ]; then
 
 	# Display help text
-	if [ ${USER_INPUT} == "help" ]; then
+	if [ $USER_INPUT == "help" ]; then
 		cat <<EOF | mo
 {{> lib/help.txt}}
 EOF
 	fi
 
 	# Display version number
-	if [ ${USER_INPUT} == "version" ]; then
+	if [ $USER_INPUT == "version" ]; then
 		printf "\nlittle version: ${LITTLE_VERSION_NUMBER}\n\n"
 	fi
 
 	# Build the site
-	if [ ${USER_INPUT} == "build" ]; then
+	if [ $USER_INPUT == "build" ]; then
 
 		${MARKDOWN} # Convert post content from markdown to html and store in a tmp file. This tmp file will getremoved at the end
 
@@ -30,38 +30,36 @@ EOF
 		MONTH=$(date +'%m')
 		DAY=$(date +'%d')
 
+		DATABASE=lib/db/${YEAR}
+		mkdir -p ${DATABASE}
+
 		PUBLISHED_CONTENT_DIR=posts/published
 		DRAFTS_CONTENT_DIR=posts/drafts
 
 		mkdir -p ${PUBLISHED_CONTENT_DIR}/${YEAR}/${MONTH}/${DAY}
-
 		MOVE_TO=${PUBLISHED_CONTENT_DIR}/${YEAR}/${MONTH}/${DAY}
-
 		mv ${DRAFTS_CONTENT_DIR}/* ${MOVE_TO}/
 
 		POST_SLUG_WITH_EXTENSION=$(ls ${MOVE_TO})
 		POST_SLUG=${POST_SLUG_WITH_EXTENSION%%.*}
 
 		cat <<EOF | mo >tmp.html
-
 {{> templates/post.mustache}}
-
 EOF
 
 		mkdir -p dist/${YEAR}/${MONTH}/${DAY}/${POST_SLUG}
-
 		DIST_POST=dist/${YEAR}/${MONTH}/${DAY}/${POST_SLUG}
-
 		mv tmp.html ${DIST_POST}/index.html
+		cp -r assets/ dist/                                                                                                                                       # Copy the contents of the assets folder to dist
+		rm -rf templates/tmp-content-template.html                                                                                                                # Delete temporary post content
 
-        cp -r assets/ dist/ # Copy the contents of the assets folder to dist
+		echo "${YEAR}-${MONTH}-${DAY}-${POST_SLUG}=(${YEAR}/${MONTH}/${DAY}/${POST_SLUG}/index.html ${MOVE_TO}/${POST_SLUG_WITH_EXTENSION})" >>${DATABASE}/db.txt # Add reference to database
 
-		rm -rf templates/tmp-content-template.html # Delete temporary post content
 	fi
 
 else
 
-# If no user input, display help text
+	# If no user input, display help text
 	cat <<EOF | mo
 {{> lib/help.txt}}
 EOF
